8251716b8e0f534a04921d0775d6e84d62b8ccc7
Konstantin Schimitzek <k.schimitzek@web.de>
2026-02-24 11:24:06 +0100

    Changed Header Colors and commented out leaderboard

--- mpa/src/components/layout/Header.tsx
+++ mpa/src/components/layout/Header.tsx
@@ -13,45 +13,56 @@ export default function Header(): JSX.Element {
   //   }
   // };
   return (
-    <div className="navbar bg-stone-300 shadow-md px-4">
+    <div className="navbar bg-stone-800 shadow-md px-4">
       <div className="navbar-start gap-6">
-        <Link to="/" className="btn btn-ghost text-2xl font-black text-amber-950 hover:scale-105 transition-transform">
+        <Link
+          to="/"
+          className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-grey-200 hover:bg-blue-600 hover:text-white transition-all duration-300 active:scale-95"
+        >
           Homepage
         </Link>
-        <div className="hidden md:flex gap-4">
+        {/* <div className="hidden md:flex gap-4">
           <Link
             to="/leaderboard"
-            className="btn btn-ghost text-2xl font-black text-amber-950 hover:scale-105 transition-transform"
+            className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-grey-200 hover:bg-blue-500 hover:text-white transition-all duration-300 active:scale-95"
           >
             Leaderboard
           </Link>
-        </div>
+        </div> */}
       </div>
 
       <div className="navbar-center hidden lg:flex">
-        <h1 className="text-xl font-bold tracking-widest uppercase text-stone-600">Welcome..Start playing !!!</h1>
+        <h1 className="text-xl font-bold tracking-widest uppercase text-grey-200">
+          Welcome to the arena!
+        </h1>
       </div>
 
       <div className="navbar-end gap-2">
-        <Link to="/roster" className="btn btn-ghost text-lg font-bold text-amber-950 hover:underline">
+        <Link
+          to="/roster"
+          className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-grey-200 hover:bg-blue-600 hover:text-white transition-all duration-300 active:scale-95"
+        >
           My Roster
         </Link>
 
         <Link
           to="/battle"
-          className="btn btn-ghost text-lg font-bold text-red-700 hover:bg-red-100 active:scale-95 transition-all"
+          className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-red-700 hover:bg-red-800 hover:text-white transition-all duration-300 active:scale-95"
         >
           Battle
         </Link>
 
         <Link
           to="/login"
-          className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-amber-950 hover:bg-blue-500 hover:text-white hover:border-blue-600 transition-all duration-300 active:scale-95"
+          className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-grey-200 hover:bg-blue-600 hover:text-white transition-all duration-300 active:scale-95"
         >
           Login
         </Link>
 
-        <Link to="/register" className="btn btn-ghost text-lg font-bold text-amber-950 hover:underline">
+        <Link
+          to="/register"
+          className="btn btn-ghost border-2 border-amber-950/20 rounded-xl text-lg font-bold text-grey-200 hover:bg-blue-600 hover:text-white transition-all duration-300 active:scale-95"
+        >
           Register
         </Link>
       </div>
